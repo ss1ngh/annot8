42d95f8491f3872d43914550517814d2dee77311
ss1ngh <singhshreyansh2354@gmail.com>
2026-01-08 02:24:12 +0530

    feat(ui): add navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Logo from "../public/annot8-logo.svg"
+import { BookOpen, Download } from "lucide-react";
+const Navbar = () => {
+
+
+    return ( 
+        <nav className="grid grid-cols-2 h-8 w-full bg-white/90 border-gray-800 px-4 items-center z-50">
+            
+            {/*Logo*/}
+            <div className=" flex items-center gap-3">
+                <div className="flex items-center cursor-pointer">
+                    <div className="w-16 h-16 flex items-center justify-center transition-transform group-hover:scale-150">
+                        <Image src={Logo} alt="annot8 logo" className="h-20 w-auto"/>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex justify-end gap-3">
+                <button
+                    // onClick={onDownload}
+                    className=" flex gap-2"
+                >
+                    <div className="flex items-center ">
+                        <BookOpen size={20}
+                        className="relative top-0.5"/>
+                    </div>
+                    <span className=" leading-tight inline-flex items-center text-md">How it works</span>               
+                </button>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar;
