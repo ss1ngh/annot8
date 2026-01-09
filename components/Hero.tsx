009e9128114085fcb478a903eb4467d9b80a810b
ss1ngh <singhshreyansh2354@gmail.com>
2026-01-09 16:49:58 +0530

    added micro interactions

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,34 +5,20 @@ import { TypeAnimation } from "react-type-animation"
 const Hero = () => {
   return (
     <section className='h-screen w-full text-center py-10'>
-        <div className='text-3xl font-extralight my-3 font-instrument-serif tracking-tight'>
+        <div className='text-4xl font-extralight py-3 font-instrument-serif tracking-tight'>
             <h3>
                 Dual-View Workspace for PDF note taking
             </h3>
         </div>
 
-        <div className='text-6xl my-4 font-sans tracking-tighter'>
+        <div className='text-6xl pt-0 py-6 font-sans  tracking-tighter'>
             <h1>
-                Read,{' '}
-                <TypeAnimation
-                    sequence={[
-                        'Doodle',
-                        3000,
-                        'Annotate',
-                        3000
-                    ]}
-                    wrapper="span"
-                    speed={20}
-                    className="font-instrument-serif inline-block"
-                    repeat={Infinity}
-                    cursor={true} 
-                />
-                , Write
+                Read<span className="font-instrument-serif">,</span> <span className="font-instrument-serif">Doodle</span><span className="font-instrument-serif">,</span> Write
             </h1>
         </div>
 
         <div className="py-6">
-            <Link href={'/upload'} className='border-2 border-black bg-black text-white rounded-3xl px-4 py-2 hover:border-white font-ibm-plex-mono cursor-pointer'>
+            <Link href={'/upload'} className='inline-block bg-black text-white rounded-3xl px-4 py-2 hover:scale-95 font-ibm-plex-mono cursor-pointer'>
                 Get Started
             </Link>
         </div>
