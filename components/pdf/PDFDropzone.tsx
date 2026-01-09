848047fac771345fbf0bd1d2c42916d7a4de1c7f
ss1ngh <singhshreyansh2354@gmail.com>
2026-01-09 18:36:51 +0530

    add pdfdropzone for file uploads

--- components/pdf/PDFDropzone.tsx
+++ components/pdf/PDFDropzone.tsx
@@ -1,22 +1,19 @@
 'use client'
 import { useCallback } from "react";
 import { useDropzone } from 'react-dropzone';
-import { useRouter } from "next/router";
 import { Upload } from "lucide-react";
 import  usePDFStore from '@/store/pdfStore';
 
  function PDFDropzone() {
-    const router = useRouter();
     const setFile = usePDFStore ((state) => state.setFile);
 
     const onDrop = useCallback(
         (acceptedFiles : File[]) => {
             if(acceptedFiles.length > 0){
                 setFile(acceptedFiles[0]);
-                router.push('/workspace');
-            }
+                }
         },
-        [setFile, router]
+        [setFile]
     );
 
     const {getRootProps, getInputProps ,isDragActive} = useDropzone({
@@ -29,17 +26,12 @@ import  usePDFStore from '@/store/pdfStore';
     <div
         {...getRootProps()}
         className={`
-            h-64 border-2 border-dashed rounded-xl flex items-center justify-center cursor-pointer transition-all duration-200
-            ${isDragActive
-                ? "border-blue-500 bg-blue-50"
-                : "border-gray-300 hover:border-blue-400"
-            }
-        `}
+            h-64 border-3 border-dashed border-gray-300 rounded-xl flex items-center justify-center cursor-pointer transition-all duration-200`}
     >
-        <input {...getInputProps} />
+        <input {...getInputProps()} />
         <div className="text-center space-y-4">
             <Upload className="w-12 h-12 mx-auto text-gray-400"/>
-            <p className="text-gray-600 text-lg">
+            <p className="text-gray-600 text-sm">
                 {isDragActive 
                 ? "Drop your PDF here"
                 : "Drag and Drop or Upload your PDF here"}
